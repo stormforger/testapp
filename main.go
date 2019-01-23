e5ba81ff6dfed268ce559763ab0b289bcf1690de
Sebastian Cohnen <tisba@users.noreply.github.com>
2019-01-23 14:10:29 +0100

    add shutdown command, add "do-not-reply" handler, add "respond with bytes" handler

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -18,27 +22,88 @@ func main() {
 		port = "9000"
 	}
 
+	shutdownCode := os.Getenv("SHUTDOWN_CODE")
+	if shutdownCode == "" {
+		logrus.Warn("SHUTDOWN_CODE not configured!")
+	}
+
+	shutdownCh := make(chan bool)
+
 	r := mux.NewRouter()
 	r.Use(delayMiddleware)
 
+	// demo router
 	s := r.PathPrefix("/demo").Subrouter()
 	s.HandleFunc("/register", registerHandler)
 	s.HandleFunc("/search", searchHandler)
 
+	// command router
+	x := r.PathPrefix("/cmd").Subrouter()
+	x.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		if shutdownCode != "" && r.URL.Query().Get("code") == shutdownCode {
+			w.Write([]byte("OK"))
+			shutdownCh <- true
+		} else {
+			w.WriteHeader(http.StatusForbidden)
+		}
+	})
+
+	// static data
 	r.PathPrefix("/data/").Handler(http.StripPrefix("/data/", http.FileServer(http.Dir("data"))))
 
-	r.PathPrefix("/").HandlerFunc(echoHandler)
+	// other handlers
+	r.HandleFunc("/respond-with/bytes", respondWithBytesHandler)
+	r.HandleFunc("/do-not-respond", doNotRespondHandler)
 
-	listenAddr := ":" + port
+	// echo handler for everything else
+	r.PathPrefix("/").HandlerFunc(echoHandler)
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         listenAddr,
+		Addr:         ":" + port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
 	logrus.Infof("Starting at :%s", port)
 
-	log.Fatal(srv.ListenAndServe())
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+	select {
+	case <-shutdownCh:
+		srv.Shutdown(context.Background())
+		logrus.Info("Shutting down on request")
+	}
+}
+
+// FIXME: this will print a stack trace to the logs, but I don't know
+//        if there is another way to kill the request (and the connection)
+//        in a way that we do not respond
+func doNotRespondHandler(w http.ResponseWriter, r *http.Request) {
+	panic("do-not-respond")
+}
+
+func respondWithBytesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/octet-stream")
+
+	sizeParam := r.URL.Query().Get("size")
+	size := 0
+	if sizeParam != "" {
+		var err error
+		size, err = strconv.Atoi(sizeParam)
+		if err != nil {
+			size = 0
+		}
+	}
+
+	w.WriteHeader(200)
+
+	data := make([]byte, size)
+	if _, err := rand.Read(data); err != nil {
+		fmt.Fprintf(w, "Could not generate random response payload")
+	}
+	w.Write(data)
 }
